126d9a56145cb2abbad56bbe45a41a72bf875d1f
Srini1496 <96612140+Srini1496@users.noreply.github.com>
2022-01-20 15:48:30 +0530

    Create Daily Temperatures.java

---
+++ Daily Temperatures.java
@@ -0,0 +1,54 @@
+/*
+LeetCode 739
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+*/
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st=new Stack<Integer>();
+        int n=temperatures.length;
+        int[] ans=new int[n];
+        for(int i=n-1;i>=0;i--){
+            if(st.isEmpty()){
+                ans[i]=0;
+                st.push(i);
+            }else if(temperatures[i]<temperatures[st.peek()]){
+               
+                ans[i]=st.peek()-i;
+                st.push(i);
+            }else{
+                while(!st.isEmpty() && temperatures[i]>=temperatures[st.peek()]){
+                    st.pop();
+                }
+                if(!st.isEmpty())
+                    ans[i]=st.peek()-i;
+                else{
+                    ans[i]=0;
+                   
+                }
+                st.push(i);
+            }
+        }
+        return ans;
+    }
+}
